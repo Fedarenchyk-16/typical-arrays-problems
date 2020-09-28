5bd90e334cf9efdc7c1b91c44232d315189d2c47
Nikita Fedarenchyk <fnikita01@mail.ru>
2020-09-28 21:30:18 +0300

    feat: make all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(arguments[0] === undefined){
+        return 0;
+    }else if(array.length === 0) {
+        return 0;
+            }else{
+        let min = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(arguments[0] === undefined){
+        return 0;
+    }else if(array.length === 0) {
+        return 0;
+    }else{
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(arguments[0] === undefined){
+        return 0;
+    }else if(array.length === 0) {
+        return 0;
+    }else{
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (sum/array.length);
+    }
 }
